2f0c38bc15558e833d260ca2e7ba5c5c71a50790
Rachit Manchanda <36093572+rachitmanchanda@users.noreply.github.com>
2019-03-24 01:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,15 @@ from flask import Flask
 import requests 
 from flask_mail import Mail, Message
 from conf import Config
+from flask import make_response
+import json
 import os 
 
 app = Flask(__name__)
 
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": Config.EMAIL_USER,
-    "MAIL_PASSWORD": Config.EMAIL_PASSWORD,
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
-
 @app.route('/webhook',methods=['POST'])
 def webhook():
-    # req= requests.get_json(silent=True, force=True)
+    req=requests.get_json(silent=True, force=True)
 
     # print("Requests:")
     # print(json.dump(req,indent=4))
@@ -31,15 +21,34 @@ def webhook():
     # res = json.dumps(res, indent=4)
     # r= make_response(res)
     # return r
-    mailSend()
+    res = mailSend()
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 def mailSend():
+    mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": Config.EMAIL_USER,
+    "MAIL_PASSWORD": Config.EMAIL_PASSWORD,
+    }
+
+    app.config.update(mail_settings)
+    mail = Mail(app)
+
     with app.app_context():
         msg = Message(subject="Hello",
             sender=app.config.get("MAIL_USERNAME"),
             recipients=["rachitmanchanda@yahoo.com"], 
             body="Hi")
         mail.send(msg)
+    return {
+        "Success": "true"
+    }
 
 
 if __name__ == '__main__':
